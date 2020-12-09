5b8cfead7066f4bc9b286acffd1c8096f7761feb
Daniël van Drunen <daniel@pixely.me>
2020-12-09 23:58:49 +0100

    Revamp Docker setup

--- docker_boot.sh
+++ docker_boot.sh
@@ -1,5 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-./wait-for-it.sh database2:3306 -- php artisan migrate
+if [ ! -z $BUDGET_SETUP ]; then
+  composer install
+  cp .env.docker .env
+  php artisan key:generate
+  php artisan storage:link
+  yarn production
+fi
 
+# Move these to Supervisor?
+service nginx start
 php-fpm
